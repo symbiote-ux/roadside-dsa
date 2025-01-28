a4bcce157c9a412cd71d633df967cd01f525643a
Saurabh Chauhan <tnikhil422@gmail.com>
2025-01-28 09:24:17 +0530

    optimised code

--- palindrome.js
+++ palindrome.js
@@ -2,7 +2,7 @@
 // 20 - not a palindrome
 
 const isPalindrome = (number) => {
-  return number === +number.toString().split('').reverse().join('');
+  return x < 0 ? number === +number.toString().split('').reverse().join('');
   // 121 -> "121" -> ['1','2','1'] -> ['1','2','1'] -> '121'
 };
 
