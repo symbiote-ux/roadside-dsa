19a926009ee9a8d83aa0716ed8a12f8ee2b494c9
Saurabh Chauhan <tnikhil422@gmail.com>
2025-01-29 15:56:52 +0530

    Greedy algorithm for maxProfit

--- bestTimeToBuySellStocks.js
+++ bestTimeToBuySellStocks.js
@@ -16,29 +16,46 @@
 
 //BruteForce solution
 
+// const maxProfit = (prices) => {
+// [7, 1, 5, 3, 6, 4];
+// 1-7, 5-7, 3-7, 6-7, 4-7 bought at 7
+// 5-1 , 3-1, 6-1, 4-1, bought at 1
+// 3-5, 6-5 , 4-5 bought at 5
+// 6-3, 4-3 bought at 3
+// 4-6 bought at 6
+
+//   let globalProfit = 0;
+//   for (let i = 0; i < prices.length - 1; i++) {
+//     for (let j = i + 1; j < prices.length; j++) {
+//       const currentProfit = prices[j] - prices[i];
+//       if (currentProfit > globalProfit) {
+//         globalProfit = currentProfit;
+//       }
+//     }
+//   }
+//   return globalProfit;
+// };
+
+// We will use greedy algorithm
+
 const maxProfit = (prices) => {
-  // [7, 1, 5, 3, 6, 4];
-  // 1-7, 5-7, 3-7, 6-7, 4-7 bought at 7
-  // 5-1 , 3-1, 6-1, 4-1, bought at 1
-  // 3-5, 6-5 , 4-5 bought at 5
-  // 6-3, 4-3 bought at 3
-  // 4-6 bought at 6
+  let minStockPurchasePrice = prices[0] || 0;
+  let profit = 0;
 
-  let globalProfit = 0;
-  for (let i = 0; i < prices.length - 1; i++) {
-    for (let j = i + 1; j < prices.length; j++) {
-      const currentProfit = prices[j] - prices[i];
-      if (currentProfit > globalProfit) {
-        globalProfit = currentProfit;
-      }
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < minStockPurchasePrice) {
+      minStockPurchasePrice = prices[i];
     }
+    profit = Math.max(profit, prices[i] - minStockPurchasePrice);
   }
-  return globalProfit;
+  return profit;
 };
-
-// We will use greedy algorithm 
-
 
 const prices = [7, 1, 5, 3, 6, 4];
 
 console.log(maxProfit(prices));
+
+// [7, 1, 5, 3, 6, 4];
+
+// min = 7 => 1 =>
+// profit = 0 => 0 => 5-1 = 4 => 6-1 = 5 =>
